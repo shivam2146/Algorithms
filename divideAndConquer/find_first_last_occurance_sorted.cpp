c8183e9326fd0eaf9911e1c8e4611ea9dc98f1bb
shivam2146 <shivam.mcs17.du@gmail.com>
2018-07-01 06:43:29 +0530

    add first and last occurance in an array

---
+++ divideAndConquer/find_first_last_occurance_sorted.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int *arr,int n,int x,bool searchFirst){
+  int first=0,last=n-1,result=-1;
+  while(first<=last){
+    int mid = (first+last)/2;
+    if(arr[mid] == x){
+      result = mid;
+      if(searchFirst)
+        last = mid-1;
+      else
+        first = mid+1;
+    }else if(arr[mid]>x)
+      last =  mid-1;
+      else
+      first =  mid+1;
+  }
+  return result;
+}
+
+int main(){
+  int arr[]= {2,5,5,5,6,6,8,9,9,9,9};
+  int n= sizeof(arr)/sizeof(arr[0]);
+  int x = 9;
+  int first = binarySearch(arr,n,x,1);
+  int last = binarySearch(arr,n,x,0);
+  cout<<"first occurance is : "<<first<<endl;
+  cout<<"last occurance is : "<<last<<endl;
+  return 0;
+}
